5f584e43cdbba9ba6f24bd0dd583a34edba89a6f
Marwan <marwan.farhat.888@gmail.com>
2024-10-05 21:12:49 +0300

    Part 03 Product Module - Entities

---
+++ Demo.Core.Domain/Entities/Products/ProductBrand.cs
@@ -0,0 +1,15 @@
+﻿using Demo.Core.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Core.Domain.Entities.Products
+{
+    public class ProductBrand:BaseEntity<int>
+    {
+        public required string Name { get; set; }
+
+    }
+}
---
+++ Demo.Core.Domain/Entities/Products/Products.cs
@@ -0,0 +1,22 @@
+﻿using Demo.Core.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Core.Domain.Entities.Products
+{
+    public class Products : BaseEntity<int>
+    {
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public  string? PictureUrl { get; set; }
+        public  decimal Price { get; set; }
+
+        public int? BrandId { get; set; }   // Foreign Key for ProductBrand    
+        public virtual ProductBrand? Brand { get; set; } // Navigational Property with ProductBrand Table  M:1   +  It's virtual to enable Lazy Loading Mode
+        public int? CategoryId { get; set; }   // Foreign Key for ProductCategory    
+        public virtual ProductCategory? Category { get; set; } // Navigational Property with ProductCategory Table  M:1   +  It's virtual to enable Lazy Loading Mode
+    }
+} 
