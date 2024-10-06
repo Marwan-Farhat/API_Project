8ce0805ccf6628e64a4b9b8f54988dadd3b40bc4
Marwan <marwan.farhat.888@gmail.com>
2024-10-06 21:31:28 +0300

    Part 08 Generic Repository

---
+++ Demo.Core.Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Core.Domain.Contracts
+{
+    public interface IGenericRepository<TEntity,TKey> where TEntity : BaseEntity<TKey> where TKey : IEquatable<TKey>
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync(bool withTracking = false);
+        Task<TEntity?> GetAsync(TKey id);
+        Task AddAsync(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
