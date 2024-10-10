c0e9ce68d553becf370a5cb5304bda6f3e2c9df6
Marwan <marwan.farhat.888@gmail.com>
2024-10-10 03:17:53 +0300

    Part 12 ProductsController - GetBrands & GetCategories endpoint

--- Demo.APIs.Controllers/Controllers/Products/ProductsController.cs
+++ Demo.APIs.Controllers/Controllers/Products/ProductsController.cs
@@ -29,5 +29,19 @@ namespace Demo.APIs.Controllers.Controllers.Products
 
             return Ok(product);
         }
+
+        [HttpGet("brands")]  // GET: /api/products/brands
+        public async Task<ActionResult<IEnumerable<BrandDto>>> GetBrands()
+        {
+            var brands = await serviceManager.ProductService.GetBrandsAsync();
+            return Ok(brands);
+        }
+
+        [HttpGet("categories")]  // GET: /api/products/brands
+        public async Task<ActionResult<IEnumerable<CategoryDto>>> GetCategories()
+        {
+            var categories = await serviceManager.ProductService.GetCategoriesAsync();
+            return Ok(categories);
+        }
     }
 }
