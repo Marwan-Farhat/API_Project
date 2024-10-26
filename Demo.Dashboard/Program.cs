74999b24f4f841261f99ab5012e3481a33f6d2fc
Marwan <marwan.farhat.888@gmail.com>
2024-10-26 13:48:57 +0300

    Part 01 Set project structure (AddDbContexts,Layout,Template Files)

--- Demo.Dashboard/Program.cs
+++ Demo.Dashboard/Program.cs
@@ -1,3 +1,10 @@
+using Demo.Core.Domain.Identity;
+using Demo.Infrastructure.Persistence.Data;
+using Demo.Infrastructure.Persistence.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
+
 namespace Demo.Dashboard
 {
     public class Program
@@ -6,10 +13,40 @@ namespace Demo.Dashboard
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            #region Configure Services
+
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+            builder.Services.AddDbContext<StoreDbContext>((optionsBuilder) =>
+            {
+                optionsBuilder
+                .UseLazyLoadingProxies()
+                .UseSqlServer(builder.Configuration.GetConnectionString("StoreContext"));
+            });
+
+            builder.Services.AddDbContext<StoreIdentityDbContext>((optionsBuilder) =>
+            {
+                optionsBuilder
+                .UseLazyLoadingProxies()
+                .UseSqlServer(builder.Configuration.GetConnectionString("IdentityContext"));
+            });
+
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(identityOptions =>
+            {
+                identityOptions.Password.RequireNonAlphanumeric = true;  // @#$%
+                identityOptions.Password.RequiredLength = 6;
+                identityOptions.Password.RequireDigit = true;
+                identityOptions.Password.RequireUppercase = true;
+                identityOptions.Password.RequireLowercase = true;              
+            })
+                            .AddEntityFrameworkStores<StoreIdentityDbContext>();
+
+            #endregion
 
             var app = builder.Build();
+
+            #region Configure Kestrel Middlwares
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
@@ -28,7 +65,9 @@ namespace Demo.Dashboard
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=Index}/{id?}"); 
+
+            #endregion
 
             app.Run();
         }
